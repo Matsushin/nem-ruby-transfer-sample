edbb4f6f9441e61f463fa4d8f77f5978f3d4e7c1
Shinichi Matsuda <matsu1983@gmail.com>
2018-08-02 22:59:36 +0900

    NEMの送金ができるように

--- app/controllers/transfers_controller.rb
+++ app/controllers/transfers_controller.rb
@@ -1,5 +1,17 @@
 class TransfersController < ApplicationController
   def create
-    # TODO 送金する
+    private_key = transfer_params[:private_key]
+    node = Nem::Node.new(host: 'bigalice2.nem.ninja')
+    kp = Nem::Keypair.new(private_key)
+    tx_endpoint = Nem::Endpoint::Transaction.new(node)
+    tx = Nem::Transaction::Transfer.new(transfer_params[:recipient], transfer_params[:amount].to_f, '送金テストです')
+    req = Nem::Request::Announce.new(tx, kp)
+    $response = tx_endpoint.announce(req)
+  end
+
+  private
+
+  def transfer_params
+    params.require(:transfer).permit(:private_key, :recipient, :amount)
   end
 end
